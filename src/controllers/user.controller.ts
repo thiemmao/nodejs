1c75b8f430d9dacc8968ee236d9781c26fb292b4
thiemmao <141142346+thiemmao@users.noreply.github.com>
2025-05-22 23:29:46 +0700

    SSR

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,22 +1,39 @@
+import { render } from 'ejs';
 import { Request, Response } from 'express';
-import { handleCreateUser,handleGetUser } from '../services/user.service';
+import { handleCreateUser,handleGetUser,handledeleteuser,handleGetUserbyid,handleupdateUser } from 'services/user.service';
 const gethomepage =async (req, res) => {
     // get user from database
     const user = await handleGetUser();
-    console.log("sss",user);
+    // console.log("sss",user);
     res.render('home',{
-        name :"user",
+        name :user,
         title: 'Home'
     });
 }
 const getcreateuserpage = (req:Request , res:Response) => {
     return res.render('create-user');
 }
-const postcreateuserpage = (req:Request , res:Response) => {
+const postcreateuserpage = async (req:Request , res:Response) => {
 
     const { email, description, address } = req.body;
-    handleCreateUser(email, description, address);
+    await handleCreateUser(email, description, address);
     return res.redirect('/');
 }
-export { gethomepage,getcreateuserpage,postcreateuserpage,handleCreateUser };
+const postdeleteuser = async  (req:Request , res:Response) => {
+    // console.log(req.params.id);
+    await handledeleteuser(req.params.id);
+    return res.redirect('/');
+}
+const getviewuserpage = async (req:Request , res:Response) => {
+    const user = await handleGetUserbyid(req.params.id);
+    return res.render('view-user',{
+        user:user
+    });
+}
+const postupdateuser = async (req:Request , res:Response) => {
+    const { id, email, description, address } = req.body;
+    await handleupdateUser(id,description,email,address);
+    return res.redirect('/');
+}
+export { gethomepage,getcreateuserpage,postcreateuserpage,handleCreateUser ,postdeleteuser,getviewuserpage,postupdateuser };
 // export { gethomepage, getcreateuserpage };
--- src/routes/wed.ts
+++ src/routes/wed.ts
@@ -1,6 +1,6 @@
 import express,{Express} from 'express';
 
-import { getcreateuserpage, gethomepage,postcreateuserpage } from '../controllers/user.controller';
+import { getcreateuserpage, gethomepage,postcreateuserpage,postdeleteuser,getviewuserpage,postupdateuser } from '../controllers/user.controller';
 const router = express.Router();
 const wedRouter =(app:Express) => {
 router.get('/', gethomepage);
@@ -9,6 +9,9 @@ router.get('/wed', (req, res) => {
   })
 router.get('/create-user',getcreateuserpage);
 router.post('/create-user',postcreateuserpage);
+router.post('/handle-delete-user/:id',postdeleteuser);
+router.get('/handle-view-user/:id',getviewuserpage);
+router.post('/update-user',postupdateuser);
   app.use('/', router);
 }
 
